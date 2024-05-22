6d42bda183f3fe26de39d9d6ac4d5756abf4c62d
arona22 <arona22@ru.is>
2024-05-22 14:07:24 +0200

    'map'

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -5,10 +5,21 @@
 #include "graphics/fbo.h"
 #include "graphics/shader.h"
 #include "framework/input.h"
+#include "framework/entities/entity.h"
+#include "graphics/material.h"
+#include "graphics/EntityMesh.h"
 
 #include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
-//some globals
+// Forward declarations
+std::vector<std::string> tokenize(const std::string& str, const std::string& delimiters);
+
+// Some globals
 Mesh* mesh = NULL;
 Texture* texture = NULL;
 Shader* shader = NULL;
@@ -17,122 +28,216 @@ float mouse_speed = 100.0f;
 
 Game* Game::instance = NULL;
 
-Game::Game(int window_width, int window_height, SDL_Window* window)
+std::unordered_map<std::string, std::vector<Matrix44>> meshes_to_load;
+
+bool parseScene(const char* filename, Entity* root)
 {
-	this->window_width = window_width;
-	this->window_height = window_height;
-	this->window = window;
-	instance = this;
-	must_exit = false;
+    std::cout << " + Scene loading: " << filename << "..." << std::endl;
 
-	fps = 0;
-	frame = 0;
-	time = 0.0f;
-	elapsed_time = 0.0f;
-	mouse_locked = false;
+    std::ifstream file(filename);
 
-	// OpenGL flags
-	glEnable( GL_CULL_FACE ); //render both sides of every triangle
-	glEnable( GL_DEPTH_TEST ); //check the occlusions using the Z buffer
+    if (!file.good()) {
+        std::cerr << "Scene [ERROR] File not found!" << std::endl;
+        return false;
+    }
 
-	// Create our camera
-	camera = new Camera();
-	camera->lookAt(Vector3(0.f,100.f, 100.f),Vector3(0.f,0.f,0.f), Vector3(0.f,1.f,0.f)); //position the camera and point to 0,0,0
-	camera->setPerspective(70.f,window_width/(float)window_height,0.1f,10000.f); //set the projection, we want to be perspective
+    std::string scene_info, mesh_name, model_data;
+    file >> scene_info >> scene_info;
+    int mesh_count = 0;
 
-	// Load one texture using the Texture Manager
-	texture = Texture::Get("data/textures/texture.tga");
+    // Read file line by line and store mesh path and model info in separated variables
+    while (file >> mesh_name >> model_data)
+    {
+        if (mesh_name[0] == '#')
+            continue;
 
-	// Example of loading Mesh from Mesh Manager
-	mesh = Mesh::Get("data/meshes/box.ASE");
+        // Get all 16 matrix floats
+        std::vector<std::string> tokens = tokenize(model_data, ",");
 
-	// Example of shader loading using the shaders manager
-	shader = Shader::Get("data/shaders/basic.vs", "data/shaders/texture.fs");
+        // Fill matrix converting chars to floats
+        Matrix44 model;
+        for (int t = 0; t < tokens.size(); ++t) {
+            model.m[t] = (float)atof(tokens[t].c_str());
+        }
 
-	// Hide the cursor
-	SDL_ShowCursor(!mouse_locked); //hide or show the mouse
+        // Add model to mesh list (might be instanced!)
+        meshes_to_load[mesh_name].push_back(model);
+        mesh_count++;
+    }
+
+    // Iterate through meshes loaded and create corresponding entities
+    for (auto& data : meshes_to_load) {
+        mesh_name = "data/" + data.first;
+        std::vector<Matrix44>& models = data.second;
+
+        // No transforms, nothing to do here
+        if (models.empty())
+            continue;
+
+        Material mat;
+        EntityMesh* new_entity = nullptr;
+
+        size_t tag = data.first.find("@tag");
+
+        if (tag != std::string::npos) {
+            Mesh* mesh = Mesh::Get("...");
+            if (!mesh) {
+                std::cerr << "Mesh loading failed (tag): " << mesh_name << std::endl;
+                continue;
+            }
+            // Create a different type of entity
+            // new_entity = new ...
+        }
+        else {
+            Mesh* mesh = Mesh::Get(mesh_name.c_str());
+            if (!mesh) {
+                std::cerr << "Mesh loading failed: " << mesh_name << std::endl;
+                continue;
+            }
+            new_entity = new EntityMesh(mesh, mat);
+        }
+
+        if (!new_entity) {
+            std::cerr << "Entity creation failed: " << data.first << std::endl;
+            continue;
+        }
+
+        new_entity->name = data.first;
+
+        // Create instanced entity
+        if (models.size() > 1) {
+            new_entity->isInstanced = true;
+            new_entity->models = models; // Add all instances
+        }
+        // Create normal entity
+        else {
+            new_entity->model = models[0];
+        }
+
+        // Add entity to scene root
+        root->addChild(new_entity);
+    }
+
+    std::cout << "Scene [OK] Meshes added: " << mesh_count << std::endl;
+    return true;
 }
 
-//what to do when the image has to be draw
+std::vector<std::string> tokenize(const std::string& str, const std::string& delimiters)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do {
+        pos = str.find_first_of(delimiters, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos - prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + 1;
+    } while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+
+Game::Game(int window_width, int window_height, SDL_Window* window)
+{
+    this->window_width = window_width;
+    this->window_height = window_height;
+    this->window = window;
+    instance = this;
+    must_exit = false;
+
+    fps = 0;
+    frame = 0;  // Ensure frame is initialized
+    time = 0.0f;
+    elapsed_time = 0.0f;
+    mouse_locked = false;
+
+    // OpenGL flags
+    glEnable(GL_CULL_FACE); //render both sides of every triangle
+    glEnable(GL_DEPTH_TEST); //check the occlusions using the Z buffer
+
+    // Create our camera
+    camera = new Camera();
+    camera->lookAt(Vector3(0.f, 100.f, 100.f), Vector3(0.f, 0.f, 0.f), Vector3(0.f, 1.f, 0.f)); //position the camera and point to 0,0,0
+    camera->setPerspective(70.f, window_width / (float)window_height, 0.1f, 10000.f); //set the projection, we want to be perspective
+
+    // Load one texture using the Texture Manager
+    texture = Texture::Get("data/textures/texture.tga");
+
+    root = new Entity();
+    parseScene("data/myscene.scene", root);
+    
+    
+
+    // Example of shader loading using the shaders manager
+    shader = Shader::Get("data/shaders/basic.vs", "data/shaders/texture.fs");
+
+    // Hide the cursor
+    SDL_ShowCursor(!mouse_locked); //hide or show the mouse
+}
+
+// what to do when the image has to be drawn
 void Game::render(void)
 {
-	// Set the clear color (the background color)
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+    // Set the clear color (the background color)
+    glClearColor(0.0, 0.0, 0.0, 1.0);
 
-	// Clear the window and the depth buffer
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Clear the window and the depth buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Set the camera as default
-	camera->enable();
+    // Set the camera as default
+    camera->enable();
 
-	// Set flags
-	glDisable(GL_BLEND);
-	glEnable(GL_DEPTH_TEST);
-	glDisable(GL_CULL_FACE);
+    // Set flags
+    glDisable(GL_BLEND);
+    glEnable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+
+
+    root->render(camera);
    
-	// Create model matrix for cube
-	Matrix44 m;
-	m.rotate(angle*DEG2RAD, Vector3(0.0f, 1.0f, 0.0f));
 
-	if(shader)
-	{
-		// Enable shader
-		shader->enable();
+    // Disable shader
+    shader->disable();
 
-		// Upload uniforms
-		shader->setUniform("u_color", Vector4(1,1,1,1));
-		shader->setUniform("u_viewprojection", camera->viewprojection_matrix );
-		shader->setUniform("u_texture", texture, 0);
-		shader->setUniform("u_model", m);
-		shader->setUniform("u_time", time);
+    // Draw the floor grid
+    drawGrid();
 
-		// Do the draw call
-		mesh->render( GL_TRIANGLES );
+    // Render the FPS, Draw Calls, etc
+    drawText(2, 2, getGPUStats(), Vector3(1, 1, 1), 2);
 
-		// Disable shader
-		shader->disable();
-	}
-
-	// Draw the floor grid
-	drawGrid();
-
-	// Render the FPS, Draw Calls, etc
-	drawText(2, 2, getGPUStats(), Vector3(1, 1, 1), 2);
-
-	// Swap between front buffer and back buffer
-	SDL_GL_SwapWindow(this->window);
+    // Swap between front buffer and back buffer
+    SDL_GL_SwapWindow(this->window);
 }
 
 void Game::update(double seconds_elapsed)
 {
-	float speed = seconds_elapsed * mouse_speed; //the speed is defined by the seconds_elapsed so it goes constant
+    float speed = static_cast<float>(seconds_elapsed * mouse_speed); //the speed is defined by the seconds_elapsed so it goes constant
 
-	// Example
-	angle += (float)seconds_elapsed * 10.0f;
+    // Example
+    angle += static_cast<float>(seconds_elapsed * 10.0f);
 
-	// Mouse input to rotate the cam
-	if (Input::isMousePressed(SDL_BUTTON_LEFT) || mouse_locked) //is left button pressed?
-	{
-		camera->rotate(Input::mouse_delta.x * 0.005f, Vector3(0.0f,-1.0f,0.0f));
-		camera->rotate(Input::mouse_delta.y * 0.005f, camera->getLocalVector( Vector3(-1.0f,0.0f,0.0f)));
-	}
+    // Mouse input to rotate the cam
+    if (Input::isMousePressed(SDL_BUTTON_LEFT) || mouse_locked) //is left button pressed?
+    {
+        camera->rotate(Input::mouse_delta.x * 0.005f, Vector3(0.0f, -1.0f, 0.0f));
+        camera->rotate(Input::mouse_delta.y * 0.005f, camera->getLocalVector(Vector3(-1.0f, 0.0f, 0.0f)));
+    }
 
-	// Async input to move the camera around
-	if (Input::isKeyPressed(SDL_SCANCODE_LSHIFT) ) speed *= 10; //move faster with left shift
-	if (Input::isKeyPressed(SDL_SCANCODE_W) || Input::isKeyPressed(SDL_SCANCODE_UP)) camera->move(Vector3(0.0f, 0.0f, 1.0f) * speed);
-	if (Input::isKeyPressed(SDL_SCANCODE_S) || Input::isKeyPressed(SDL_SCANCODE_DOWN)) camera->move(Vector3(0.0f, 0.0f,-1.0f) * speed);
-	if (Input::isKeyPressed(SDL_SCANCODE_A) || Input::isKeyPressed(SDL_SCANCODE_LEFT)) camera->move(Vector3(1.0f, 0.0f, 0.0f) * speed);
-	if (Input::isKeyPressed(SDL_SCANCODE_D) || Input::isKeyPressed(SDL_SCANCODE_RIGHT)) camera->move(Vector3(-1.0f,0.0f, 0.0f) * speed);
+    // Async input to move the camera around
+    if (Input::isKeyPressed(SDL_SCANCODE_LSHIFT)) speed *= 10; //move faster with left shift
+    if (Input::isKeyPressed(SDL_SCANCODE_W) || Input::isKeyPressed(SDL_SCANCODE_UP)) camera->move(Vector3(0.0f, 0.0f, 1.0f) * speed);
+    if (Input::isKeyPressed(SDL_SCANCODE_S) || Input::isKeyPressed(SDL_SCANCODE_DOWN)) camera->move(Vector3(0.0f, 0.0f, -1.0f) * speed);
+    if (Input::isKeyPressed(SDL_SCANCODE_A) || Input::isKeyPressed(SDL_SCANCODE_LEFT)) camera->move(Vector3(1.0f, 0.0f, 0.0f) * speed);
+    if (Input::isKeyPressed(SDL_SCANCODE_D) || Input::isKeyPressed(SDL_SCANCODE_RIGHT)) camera->move(Vector3(-1.0f, 0.0f, 0.0f) * speed);
 }
 
-//Keyboard event handler (sync input)
-void Game::onKeyDown( SDL_KeyboardEvent event )
+// Keyboard event handler (sync input)
+void Game::onKeyDown(SDL_KeyboardEvent event)
 {
-	switch(event.keysym.sym)
-	{
-		case SDLK_ESCAPE: must_exit = true; break; //ESC key, kill the app
-		case SDLK_F1: Shader::ReloadAll(); break; 
-	}
+    switch (event.keysym.sym)
+    {
+    case SDLK_ESCAPE: must_exit = true; break; //ESC key, kill the app
+    case SDLK_F1: Shader::ReloadAll(); break;
+    }
 }
 
 void Game::onKeyUp(SDL_KeyboardEvent event)
@@ -140,14 +245,14 @@ void Game::onKeyUp(SDL_KeyboardEvent event)
 
 }
 
-void Game::onMouseButtonDown( SDL_MouseButtonEvent event )
+void Game::onMouseButtonDown(SDL_MouseButtonEvent event)
 {
-	if (event.button == SDL_BUTTON_MIDDLE) //middle mouse
-	{
-		mouse_locked = !mouse_locked;
-		SDL_ShowCursor(!mouse_locked);
-		SDL_SetRelativeMouseMode((SDL_bool)(mouse_locked));
-	}
+    if (event.button == SDL_BUTTON_MIDDLE) //middle mouse
+    {
+        mouse_locked = !mouse_locked;
+        SDL_ShowCursor(!mouse_locked);
+        SDL_SetRelativeMouseMode((SDL_bool)(mouse_locked));
+    }
 }
 
 void Game::onMouseButtonUp(SDL_MouseButtonEvent event)
@@ -157,7 +262,7 @@ void Game::onMouseButtonUp(SDL_MouseButtonEvent event)
 
 void Game::onMouseWheel(SDL_MouseWheelEvent event)
 {
-	mouse_speed *= event.y > 0 ? 1.1f : 0.9f;
+    mouse_speed *= event.y > 0 ? 1.1f : 0.9f;
 }
 
 void Game::onGamepadButtonDown(SDL_JoyButtonEvent event)
@@ -173,9 +278,8 @@ void Game::onGamepadButtonUp(SDL_JoyButtonEvent event)
 void Game::onResize(int width, int height)
 {
     std::cout << "window resized: " << width << "," << height << std::endl;
-	glViewport( 0,0, width, height );
-	camera->aspect =  width / (float)height;
-	window_width = width;
-	window_height = height;
+    glViewport(0, 0, width, height);
+    camera->aspect = width / (float)height;
+    window_width = width;
+    window_height = height;
 }
-
--- src/game/game.h
+++ src/game/game.h
@@ -1,47 +1,48 @@
-/*  by Javi Agenjo 2013 UPF  javi.agenjo@gmail.com
-	This class encapsulates the game, is in charge of creating the game, getting the user input, process the update and render.
-*/
+#ifndef GAME_H
+#define GAME_H
 
-#pragma once
-
-#include "framework/includes.h"
-#include "framework/camera.h"
 #include "framework/utils.h"
+#include "graphics/mesh.h"
+#include "graphics/texture.h"
+#include "graphics/shader.h"
+#include "framework/entities/entity.h"
+#include "graphics/material.h"
+#include "framework/input.h"
+#include "framework/camera.h"  // Include Camera header
+
+#include <SDL2/SDL.h>
 
 class Game
 {
 public:
-	static Game* instance;
+    static Game* instance;
+    Shader* shader; // Declare shader here
 
-	//window
-	SDL_Window* window;
-	int window_width;
-	int window_height;
+    Game(int window_width, int window_height, SDL_Window* window);
+    void render();
+    void update(double seconds_elapsed);
+    void onKeyDown(SDL_KeyboardEvent event);
+    void onKeyUp(SDL_KeyboardEvent event);
+    void onMouseButtonDown(SDL_MouseButtonEvent event);
+    void onMouseButtonUp(SDL_MouseButtonEvent event);
+    void onMouseWheel(SDL_MouseWheelEvent event);
+    void onGamepadButtonDown(SDL_JoyButtonEvent event);
+    void onGamepadButtonUp(SDL_JoyButtonEvent event);
+    void onResize(int width, int height);
 
-	//some globals
-	long frame;
-    float time;
-	float elapsed_time;
-	int fps;
-	bool must_exit;
+    bool must_exit; // Make public
+    float time; // Make public
+    float frame;
+    float elapsed_time; // Make public
+    float fps; // Make public
+    int window_width; // Make public
+    int window_height; // Make public
+    SDL_Window* window; // Make public
+    Camera* camera; // Ensure camera is declared here
+    Entity* root; // Add root entity for the scene
 
-	//some vars
-	Camera* camera; //our global camera
-	bool mouse_locked; //tells if the mouse is locked (not seen)
-
-	Game( int window_width, int window_height, SDL_Window* window );
-
-	//main functions
-	void render( void );
-	void update( double dt );
-
-	//events
-	void onKeyDown( SDL_KeyboardEvent event );
-	void onKeyUp(SDL_KeyboardEvent event);
-	void onMouseButtonDown( SDL_MouseButtonEvent event );
-	void onMouseButtonUp(SDL_MouseButtonEvent event);
-	void onMouseWheel(SDL_MouseWheelEvent event);
-	void onGamepadButtonDown(SDL_JoyButtonEvent event);
-	void onGamepadButtonUp(SDL_JoyButtonEvent event);
-	void onResize(int width, int height);
+private:
+    bool mouse_locked;
 };
+
+#endif
